0d1e702f35b2c89b26714df171d01d78afbcf093
Julius-TKU <Julius1900544@gmail.com>
2019-12-16 16:05:29 +0800

    Updates

---
+++ precache-manifest.a32e6529f2855347936b97b597a96a1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42abeefa8234c87c98bba13819369b54",
+    "url": "/ts-kreia/index.html"
+  },
+  {
+    "revision": "1b9d619ea839b4e2ecc0",
+    "url": "/ts-kreia/static/css/2.94aac401.chunk.css"
+  },
+  {
+    "revision": "1b9d619ea839b4e2ecc0",
+    "url": "/ts-kreia/static/js/2.ee80e8fe.chunk.js"
+  },
+  {
+    "revision": "d5047a75e2c6cc7d70f2b9e55ee153ce",
+    "url": "/ts-kreia/static/js/2.ee80e8fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3536b5a039ed717b0b20",
+    "url": "/ts-kreia/static/js/main.05a1867b.chunk.js"
+  },
+  {
+    "revision": "ead3e53e3f139da15e44",
+    "url": "/ts-kreia/static/js/runtime-main.de390958.js"
+  },
+  {
+    "revision": "227eb1d906ac8e3bd5fdcabaec92321f",
+    "url": "/ts-kreia/static/media/post.227eb1d9.md"
+  }
+]);
